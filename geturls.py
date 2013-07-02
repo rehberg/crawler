c491c486c7a3b45009b14a344f8e753393b1bfb1
Ben Rehberg <github@rtech.co>
2013-07-01 23:10:51 -0400

    Added robots.txt procedures, not included in main() yet
    Actually may have broken it.  Will keep working...

--- geturls.py
+++ geturls.py
@@ -1,10 +1,11 @@
+#!/usr/bin/python3
 #-------------------------------------------------------------------------------
 # Name:        geturls.py
 # Purpose:     Grabs URLs from pages
-#
-# Author:      Ben Rehberg
-#
-# Created:     July 1, 2013
+# Platform:    written on Python 3.2.3
+# Author:      Ben Rehberg, Two Leg Software
+#	       ben@twoleg.com
+# Created:     01/07/2013
 # Copyright:   (c) Ben Rehberg 2013
 # More Information: http://www.twoleg.com, if I bothered to publish something there...
 # Licence:     GPL
@@ -17,11 +18,11 @@ def main():
     url = 'http://www.benrehberg.com'
     page = get_page(url)
     if page:
-        links = print_all_links(get_page(url))
+        links = write_links_to_file(get_page(url))
         for link in links:
             content = get_page(link)
             if content:
-                print_all_links(content)
+                write_links_to_file(content)
 
 def get_page(url):
     """Takes a <string> url and returns a <string>
@@ -36,7 +37,24 @@ def get_page(url):
         return None
     return page
 
+
+def write_links_to_file(page):
+    a = []
+    f = open('links.txt', 'a')
+    while True:
+        url, endpos = next_http_target(page)
+        if url:
+            a.append(url)
+            f.write(url + '\n')
+            f.flush()
+            page = page[endpos:]
+        else:
+            f.close()
+            return a
+
 def next_http_target(page):
+    """Takes a <string> page and picks all the '<a href=' text elements
+    from it.  Only returns those beginning with 'http'"""
     start_link = page.find('<a href=')
     if start_link == -1:
         return None, 0
@@ -74,22 +92,8 @@ def print_all_links(page):
             return a
 
 def create_header():
-    return {'User-Agent':'TwoLeg-Crawler/1.0 Python-urllib/3.3'}
-
-def base_url(url):
-    """Returns the root URL from a given url, ending
-    with a slash just after the tld."""
-    start = url.find('://')
-    slash = url.find('/', start+3)
-    return url[:slash+1]
-
-def can_scrape(url):
-    """Takes a url and looks for robots.txt.
-    Returns true if robots.txt allows crawling."""
-    rp = urllib.robotparser.RobotFileParser()
-    rp.set_url(base_url(url)+'robots.txt')
-    rp.read()
-    rp.can_fetch('*', url)
+    header = {'User-Agent':'TwoLeg-Crawler/1.0 Python-urllib/3.3'}
 
 if __name__ == '__main__':
     main()
+
